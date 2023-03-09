0445a74064130cee585c3d13ad4dbfc7cb593e4a
AfrinaDipti <diptiafrina24@gmail.com>
2023-03-10 03:28:40 +0600

    Add madlib project

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj=input("Adjective: ")
+adv=input("Adverb: ")
+noun=input("Noun: ")
+verb=input("Verb: ")
+
+madlib=f"Memories, even your most {adj} ones, fade surprisingly {adv}. But I don't go along with that. The memories I {noun} most, \
+I don't ever see them {verb}."
+
+print(madlib)
